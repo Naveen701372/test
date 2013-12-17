7f4059dd3e3dc633dfd6b73eb46adc3506eb67a7
Navi <navi93@ubuntu.(none)>
2013-12-16 23:53:38 -0800

    test2 1

---
+++ cloudant.py
@@ -0,0 +1,21 @@
+
+
+
+class Cloudant:
+    
+    def __init__():
+        pass
+        
+    def create():
+        pass
+        
+    def read():
+        pass
+
+    def update():
+        pass
+    
+    def delete():
+        pass
+    
+        
